dfd392762a606b46868cea296cc6b8006ae12018
sthpranaya07 <sthpranaya07@gmail.com>
2025-09-20 12:23:51 +0545

    better usage of dictionary outside the class

--- student_manager/student_manager.py
+++ student_manager/student_manager.py
@@ -1,51 +1,51 @@
 from time import sleep
 
 class Student:
-    def __init__(self, name, marks, students):
+    def __init__(self, name, marks):
         self.name = name
         self.marks = marks
-        self.students = students
 
 
-    def add_student(self, name, marks):
-        self.students[name] = marks
+    def add_student(self, name, marks, my_dictionary):
+        my_dictionary[name] = marks
         print("\nSuccessfully added a student.")
 
 
-    def search_student(self, name):
-        if name in self.students:
-            print(f"\nName: {name}\nMarks: {self.students[name]}")
+    def search_student(self, name, my_dictionary):
+        if name in my_dictionary:
+            print(f"\nName: {name}\nMarks: {my_dictionary[name]}")
         else:
             print("Student not found!")
 
     
-    def update_marks(self, name, marks):
-        if name in self.students:
-            self.students[name] = marks
+    def update_marks(self, name, marks, my_dictionary):
+        if name in my_dictionary:
+            my_dictionary[name] = marks
             print("\nSuccessfully updated the marks.")
         else:
             print("Student not found!")
 
 
-    def delete_student(self, name):
-        if name in self.students:
-            del self.students[name]
+    def delete_student(self, name, my_dictionary):
+        if name in my_dictionary:
+            del my_dictionary[name]
             print("\nSuccessfully deleted the student.")
         else:
             print("Student not found!")
 
 
-    def display_all_students(self):
+    def display_all_students(self, my_dictionary):
         print()
         SN = 1
 
-        for name, marks in self.students.items():
+        for name, marks in my_dictionary.items():
             print(f"{SN}. Name: {name}\tMarks: {marks}\n")
             SN += 1
 
 
 def main():
-    student = Student("", 0, {})
+    students = {}
+    student = Student("", 0)
 
     while True:
         display()
@@ -56,22 +56,22 @@ def main():
             name = input("\nEnter name: ").strip().title()
             marks = int(input("Enter marks: "))
 
-            student.add_student(name, marks)
+            student.add_student(name, marks, students)
         elif choice == '2':
             name = input("\nEnter name: ").strip().title()
 
-            student.search_student(name)
+            student.search_student(name, students)
         elif choice == '3':
             name = input("\nEnter name: ").strip().title()
             marks = int(input("Enter new marks: "))
 
-            student.update_marks(name, marks)
+            student.update_marks(name, marks, students)
         elif choice == '4':
             name = input("\nEnter name: ").strip().title()
 
-            student.delete_student(name)
+            student.delete_student(name, students)
         elif choice == '5':
-            student.display_all_students()
+            student.display_all_students(students)
         elif choice == '6':
             print("\nExiting the program!")
             sleep(1.5)
